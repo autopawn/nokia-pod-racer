9e83b7821958fe5e13f39dc13a27c8d546c2b661
Francisco Casas <franciscojacb@gmail.com>
2023-02-26 16:48:50 -0300

    Change music to track2.

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -66,7 +66,7 @@ int main(void)
 
     // Load global data (assets that must be available in all screens, i.e. font)
     font = LoadFont("resources/mecha.png");
-    music = LoadMusicStream("resources/music.mp3");
+    music = LoadMusicStream("resources/music2.mp3");
     fxCoin = LoadSound("resources/coin.wav");
 
     nokiaScreen = LoadRenderTexture(SCREEN_W, SCREEN_H);
