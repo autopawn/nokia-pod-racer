2c2c54b01264ceb0b74f2aff3e83e2af943853d9
Francisco Casas <franciscojacb@gmail.com>
2023-02-26 04:09:56 -0300

    Draw player.

--- src/screen_gameplay.c
+++ src/screen_gameplay.c
@@ -26,51 +26,83 @@
 #include "raylib.h"
 #include "screens.h"
 
+#include <string.h>
+
 //----------------------------------------------------------------------------------
 // Module Variables Definition (local)
 //----------------------------------------------------------------------------------
 static int framesCounter = 0;
 static int finishScreen = 0;
 
+static Texture2D textureDriver;
+
+typedef struct
+{
+    float x, y;
+    int turbo_l, turbo_r;
+} Player;
+
+static Player player;
 //----------------------------------------------------------------------------------
 // Gameplay Screen Functions Definition
 //----------------------------------------------------------------------------------
 
+static void DrawTile(Texture2D tex, int tile_size_x, int tile_size_y, int tile_x, int tile_y,
+        int pos_x, int pos_y)
+{
+    Rectangle src = {tile_size_x*tile_x, tile_size_y*tile_y, tile_size_x, tile_size_y};
+
+    DrawTextureRec(tex, src, (Vector2){pos_x, pos_y}, WHITE);
+}
+
 // Gameplay Screen Initialization logic
 void InitGameplayScreen(void)
 {
-    // TODO: Initialize GAMEPLAY screen variables here!
     framesCounter = 0;
     finishScreen = 0;
+
+    textureDriver = LoadTexture("resources/driver.png");
+
+    memset(&player, 0, sizeof(player));
 }
 
 // Gameplay Screen Update logic
 void UpdateGameplayScreen(void)
 {
-    // TODO: Update GAMEPLAY screen variables here!
+    if (IsKeyDown(KEY_A))
+        player.turbo_l = 2;
+    else if (IsKeyDown(KEY_Z))
+        player.turbo_l = 1;
+    else
+        player.turbo_l = 0;
 
-    // Press enter or tap to change to ENDING screen
-    if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-    {
-        finishScreen = 1;
-        PlaySound(fxCoin);
-    }
+    if (IsKeyDown(KEY_K))
+        player.turbo_r = 2;
+    else if (IsKeyDown(KEY_M))
+        player.turbo_r = 1;
+    else
+        player.turbo_r = 0;
+
+    // // Press enter or tap to change to ENDING screen
+    // if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+    // {
+    //     finishScreen = 1;
+    //     PlaySound(fxCoin);
+    // }
 }
 
 // Gameplay Screen Draw logic
 void DrawGameplayScreen(void)
 {
-    // TODO: Draw GAMEPLAY screen here!
-    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), PURPLE);
-    Vector2 pos = { 20, 10 };
-    DrawTextEx(font, "GAMEPLAY SCREEN", pos, font.baseSize*3.0f, 4, MAROON);
-    DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+    DrawTile(textureDriver, 12, 12, player.turbo_l, player.turbo_r, 36, 36);
+    DrawTile(textureDriver, 12, 12, 3, player.turbo_l, 36 - 10, 36);
+    DrawTile(textureDriver, 12, 12, 4, player.turbo_r, 36 + 10, 36);
 }
 
 // Gameplay Screen Unload logic
 void UnloadGameplayScreen(void)
 {
-    // TODO: Unload GAMEPLAY screen variables here!
+    UnloadTexture(textureDriver);
 }
 
 // Gameplay Screen should finish?
